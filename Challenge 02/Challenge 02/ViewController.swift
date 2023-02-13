335e4abad64c988369d672e02084bb3cbcf904d8
Lucas Reis <lucasbarroreis@gmail.com>
2023-02-13 15:26:56 -0300

    Challenge

---
+++ Challenge 02/Challenge 02/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Challenge 02
+//
+//  Created by Lucas da Silva Reis on 13/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    var count = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        view.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
+        
+        button1.layer.borderColor = UIColor.gray.cgColor
+        button2.layer.borderColor = UIColor.gray.cgColor
+        button3.layer.borderColor = UIColor.gray.cgColor
+        
+        askQuestion()
+        
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        count += 1
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "\(countries[correctAnswer].uppercased()) | Score: \(score)"
+        
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        let selected = countries[sender.tag].uppercased()
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        }else {
+            title = "Wrong! This is the flag of \(selected)"
+            score -= 1
+        }
+        
+        var ac: UIAlertController!
+        
+        if count == 10{
+            ac = UIAlertController(title: "Game over", message: "Score: \n \(score)/10", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            score = 0
+            count = 0
+        }else {
+            ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        }
+        
+    }
+}
+
