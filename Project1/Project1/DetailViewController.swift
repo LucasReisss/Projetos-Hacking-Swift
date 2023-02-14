5830097e84deb63bf7674fa799eb211305f71c15
Lucas Reis <lucasbarroreis@gmail.com>
2023-02-14 10:19:09 -0300

    Share image

--- Project1/Project1/DetailViewController.swift
+++ Project1/Project1/DetailViewController.swift
@@ -15,11 +15,15 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         title = selectedImage
+
         navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
         
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -31,14 +35,18 @@ class DetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = true
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func shareTapped() {
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+        print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        
+        vc.popoverPresentationController?.barButtonItem
+        present(vc, animated: true)
     }
-    */
 
 }
