bd97d593f1141fbcb77654e3ce575dcfc5178b32
Lucas Reis <lucasbarroreis@gmail.com>
2023-02-15 14:01:19 -0300

    Challenge rightBarButtonItem

--- Challenge 02/Challenge 02/ViewController.swift
+++ Challenge 02/Challenge 02/ViewController.swift
@@ -21,6 +21,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named: "eye"), style: .plain, target: self, action: #selector(scoreTapped))
+        
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
         button1.layer.borderWidth = 1
@@ -76,6 +79,12 @@ class ViewController: UIViewController {
             present(ac, animated: true)
         }
         
+    }
+    
+    @objc func scoreTapped() {
+        var ac = UIAlertController(title: "Score", message: "\(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Close", style: .default))
+        present(ac, animated: true)
     }
 }
 
