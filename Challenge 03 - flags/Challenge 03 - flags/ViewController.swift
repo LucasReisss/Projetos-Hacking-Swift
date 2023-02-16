1fd7019efee1def8cb57a58cfbef9b1c4955b89b
Lucas Reis <lucasbarroreis@gmail.com>
2023-02-16 11:03:08 -0300

    listando imagens na tabela

---
+++ Challenge 03 - flags/Challenge 03 - flags/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Challenge 03 - flags
+//
+//  Created by Lucas da Silva Reis on 16/02/23.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("flags"){
+                pictures.append(item)
+            }
+        }
+        pictures = pictures.sorted()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+
+}
+
