2900f4c1de72c9707055b33f36bc4290d1808bbf
Lucas Reis <lucasbarroreis@gmail.com>
2023-03-07 06:13:21 -0300

    usando JSON e loadHTMLString

---
+++ Project07/Project07/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project07
+//
+//  Created by Lucas da Silva Reis on 06/03/23.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Project07/Project07/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project07
+//
+//  Created by Lucas da Silva Reis on 06/03/23.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
